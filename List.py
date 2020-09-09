43b9922f005478124a904a1df2c67ab8185e4e89
surendrasoni26 <surendra.soni1198@gmail.com>
2020-09-09 21:24:54 +0530

    list n flow control

--- List.py
+++ List.py
@@ -1,9 +1,9 @@
 players=['mahesh', 'ravi', 'dinesh', 'Suresh', 'Ganesh']
-print players[0]
-print players[:]
-print players[::]
-print players[:3]
-print players[-3:]
+print (players[0])
+print (players[:])
+print (players[::])
+print (players[:3])
+print (players[-3:])
 print("Here are first 3 players in my team")
 for player in players[:3]:
-  print (players.title())
+  print (player.title())
