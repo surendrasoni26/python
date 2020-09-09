cfe27e84b5b8cd93175c1eca712a14c2d83cb1d5
surendrasoni26 <surendra.soni1198@gmail.com>
2020-09-09 21:14:28 +0530

    list n flow control

---
+++ Flow_Control.py
@@ -0,0 +1,8 @@
+name='Marry'
+password='Jaidev'
+if name=='Marry':
+    print('Hello '+name)
+    if password=='Jaidev':
+        print('Access Granted')
+    else:
+        print("Access Denied: enter coorect password")
